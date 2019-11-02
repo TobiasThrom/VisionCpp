c2d3f15971ec3ab5cce4cb1d4baa81d1b69f2c40
Tobias <Tobias.Throm@gmx.de>
2019-11-02 12:56:26 +0100

    inital commit to VisionCpp repo

---
+++ main.cpp
@@ -0,0 +1,15 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 02.11.2019
+ *      Author: Tobias
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	cout << "Hello World" << endl;
+}
+
